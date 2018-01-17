7f80ad08776f2dd86e6e806cfd97206214e92c2e
Kenneth Fowler <kenfowler@comcast.net>
2018-01-17 23:34:27 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.nahttps://learn.co/tracks/accelerated-prep/week-2/programming-js-in-the-ide/practicing-conditional-statements#me === "Susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
